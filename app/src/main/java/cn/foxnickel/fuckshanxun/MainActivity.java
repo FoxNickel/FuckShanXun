7f0a0f6602d2c7f9e245fe472b33035aa4e21658
NickelFox <nickelfox@foxmail.com>
2017-11-16 22:04:17 +0800

    移除了SupportLibrary的依赖，apk大小降到了42kb

--- app/src/main/java/cn/foxnickel/fuckshanxun/MainActivity.java
+++ app/src/main/java/cn/foxnickel/fuckshanxun/MainActivity.java
@@ -2,21 +2,22 @@ package cn.foxnickel.fuckshanxun;
 
 import android.Manifest;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,7 +29,7 @@ import java.util.List;
 
 import cn.foxnickel.fuckshanxun.receiver.SmsReceiver;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private static final int REQUEST_PERMISSIONS = 1;
     private final String TAG = getClass().getSimpleName();
@@ -39,7 +40,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_second);
+
+        setTransparentStatusBar();
 
         sharedPreferences = getSharedPreferences("shared_preferences_pass", MODE_PRIVATE);
 
@@ -95,6 +99,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void setTransparentStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
     /**
      * 打开应用权限管理页面（小米手机会用到）
      */
@@ -135,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             Log.i(TAG, "onRequestPermissionsResult: card: " + sharedPreferences.getInt("card", 0));
             sendSMS();
